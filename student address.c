9e4b2da49c7a34d561e83e4a4e7c0520b8faf62d
harishwarner773-jpg <harishwarner773@gmail.com>
2026-01-28 09:48:03 +0530

    Add files via upload

---
+++ student address.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+struct student{
+    int ID;
+    char name[20];
+};
+struct address{
+    int pincode;
+    char cityname[20];
+    struct student name;
+};
+int main() {
+struct student std = {24,"subi"};
+struct address stud1 = {625526,"theni"};
+struct student *point = &std;
+struct address *point1 = &stud1;
+printf("my name is %s \n",point->name) ;
+printf("my ID is %d\n",point->ID);
+printf("%d\n",point1->pincode);
+printf("%s\n",point1->cityname);
+
+    return 0;
